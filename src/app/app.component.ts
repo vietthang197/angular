c4f4f9a797abd90775478e08b4fee3c1de64ff91
vietthang197 <levietthang1997@gmail.com>
2021-03-30 11:52:55 +0700

    oke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lê Việt Thắng khoai to';
+  dataUnknow: unknown;
+
   _toggleData = true;
 
   set toggleData(val) {
@@ -19,5 +21,49 @@ export class AppComponent {
 
   toggleButton = () => {
     this.toggleData = !this.toggleData;
+    let dogServiceImpl = new DogServiceImpl();
+    console.log(dogServiceImpl.findAll());
+    console.log(dogServiceImpl.findOne());
+  }
+
+  public testNever(con: boolean): string | never {
+    if (con) {
+      throw new Error('mother fucker');
+    } else {
+      return 'oke em';
+    }
   }
 }
+
+interface BaseService<T> {
+  findOne(): T;
+  findAll(): T[];
+}
+
+interface Dog {
+  name: string;
+}
+
+export class DogServiceImpl implements BaseService<Dog>{
+  findAll(): Dog[] {
+    let listDog: Dog[] = [];
+    let dog1: Dog = {
+      name: 'Day la con cho 1'
+    };
+    let dog2: Dog = {
+      name: 'Day la con cho 2'
+    };
+    listDog.push(dog1);
+    listDog.push(dog2);
+    return listDog;
+  }
+
+  findOne(): Dog {
+    let dog: Dog = {
+      name: 'Day la con cho'
+    };
+    return dog;
+  }
+}
+
+
